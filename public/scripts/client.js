6df9502b5ae32caf37d97301b9ddde9e91408b0e
Tiffany Love <tnlove_99@yahoo.com>
2017-04-29 18:20:29 -0500

    added basic structure of html and server.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,9 @@
+
+$(document).ready(onReady);
 console.log('JQ sourced');
+
+function onReady(){
+
+console.log('ready');
+
+}//end of onReady function
--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 //requires
+var express = require ('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var pg = require('pg');
 
-
-
+//globals
+var port = 4000;
 
 // set up config for the pool
-
+var config = {
+  database: 'todo',
+  host: 'localhost',
+  port: 5432,
+  max: 10
+};
+//uses
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // create new pool using config
-
-
-//static folder
-
+var pool = new pg.Pool(config);
 
 //spin up the server
-
+app.listen(port, function(){
+  console.log('server up on', port);
+});
 
 //base url
+app.get('/', function(req, res){
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/views/index.html'));
+});
