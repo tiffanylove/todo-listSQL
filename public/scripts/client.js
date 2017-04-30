51b9d80bf3053e56798218c192eedcfa809b260e
Tiffany Love <tnlove_99@yahoo.com>
2017-04-29 19:30:31 -0500

    can now add tasks to database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,36 @@ $(document).ready(onReady);
 console.log('JQ sourced');
 
 function onReady(){
+  console.log('ready');
 
-console.log('ready');
+$('#submitTask').on('click', addTask);
+
+
+
+function addTask(){
+  // will collect info from input into an object to be sent to DB.
+
+  var taskObject = {
+    newTask: $('#tasks').val()
+  };
+  console.log('new task added');
+
+$.ajax({
+  url: '/addTask',
+  type: 'POST',
+  data: taskObject,
+  success:function(data){
+    console.log('added task:', data);
+  }//end success
+});// end of ajax post
+}// end addTask function
+
+
+
+
+
+
+
+
 
 }//end of onReady function
--- server.js
+++ server.js
@@ -32,3 +32,26 @@ app.get('/', function(req, res){
   console.log('base url hit');
   res.sendFile(path.resolve('public/views/index.html'));
 });
+
+app.post('/addTask', function(req, res){
+  console.log('addTask route hit');
+
+  var taskObject = {
+    response:('from addTask route', req.body)
+
+  };//end taskObject
+  pool.connect(function(err, connection, done){
+    if(err){
+      console.log(err);
+      //respond
+      res.send(400);
+    } else{
+      console.log('connected');
+    } //end else
+
+connection.query("INSERT into todolist (tasks) values ($1)", [req.body.newTask]);
+res.send(taskObject);
+done();
+
+  }); //end pool.connect
+}); //end app.post
